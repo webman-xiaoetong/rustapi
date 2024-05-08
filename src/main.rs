1aef6e907c55ddf62eb25e602684cb244b6f3335
franscheng <franscheng@xiaoe-tech.com>
2024-05-08 18:44:25 +0800

    init web framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use salvo::prelude::*;
+
+#[handler]
+async fn hello(_req: &mut Request, _depot: &mut Depot, res: &mut Response) {
+    res.render(Text::Plain("Hello World"));
+}
+
+#[tokio::main]
+async fn main() {
+    let mut router = Router::new().get(hello);
+    let acceptor = TcpListener::new("127.0.0.1:8787").bind().await;
+    Server::new(acceptor).serve(router).await;
 }
